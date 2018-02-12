2d88347e3f5593561dc64ea5a3187a287e0d8734
Dariusz Markowicz <dmarkowicz@outlook.com>
2018-02-12 14:55:16 +0100

    set state for input value and handle function

--- things-todo/src/ToDoInput.js
+++ things-todo/src/ToDoInput.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 class ToDoInput extends Component {
+    constructor(props){
+        super(props);
+        this.state = {inputValue: 'sometext'};
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e){
+        this.setState({
+            inputValue: e.target.value
+        });
+    }
+
     render() {
         return (
             <div>
-                <input type="text"/>
+                <input 
+                    type="text" 
+                    value={this.state.inputValue}
+                    onChange={this.handleChange}
+                />
                 <button>Add</button>
             </div>
         )

a213ed559daba1aa0d181150a21804eedd0f12a2
vahus <darmarkowicz@gmail.com>
2018-02-12 14:10:06 +0100

    preparing workspace for building full stack app

---
+++ things-todo/src/ToDoInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ToDoInput extends Component {
+    render(){
+        return (
+            <h2>hello</h2>
+        )
+    }
+}
+
+export default ToDoInput;
